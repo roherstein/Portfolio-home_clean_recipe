3f7a700a84e8d75e6ac74cd5e3d5415f7a092497
EC2 Default User <ec2-user@ip-172-31-28-224.ap-northeast-1.compute.internal>
2023-02-18 15:06:37 +0000

    [add]全てのコントローラーとビューページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users,controllers:{
-    registrations: "public/registrations",
+    registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
-  
   
   devise_scope :user do
     post 'public/guest_sign_in', to: 'public/sessions#new_guest'
@@ -14,6 +13,10 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   
-  root to: "homes#top"
+  root to: 'homes#top'
+  resources :users, only:[:show,:edit,:update]
+  get 'users/mypage' => 'public/users#mypage'
+  get 'users/confirm' => 'public/users#confirm'
+  get 'users/withdrawal' => 'public/users#withdrawal'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
