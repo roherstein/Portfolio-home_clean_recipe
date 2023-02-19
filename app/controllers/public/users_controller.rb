5b71be6ca259e04572ef5adadfe615f7d1f56508
EC2 Default User <ec2-user@ip-172-31-28-224.ap-northeast-1.compute.internal>
2023-02-19 13:25:52 +0000

    [add]トップページの作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  has_one_attached :profile_image
+  
   def mypage
     @user = User.find(current_customer.id)
     @posts = Post.find(current_customer.id)
