07a0a050c35a8638444bc0f10f4abdd747d2f69a
EC2 Default User <ec2-user@ip-172-31-28-224.ap-northeast-1.compute.internal>
2023-03-16 13:22:05 +0000

    [update]レイアウト変更/トップページ/カテゴリページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   
   validates :email, presence: true
+  validates :password, presence: true, length:{minimum: 6 }
   
   def self.looks(keyword)
     @user = User.where("user_name LIKE?","%#{keyword}%")
