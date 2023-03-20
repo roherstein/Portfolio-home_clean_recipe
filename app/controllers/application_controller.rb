05f5c3e463e400b80fa035d928bcd6cce9d5b7f0
EC2 Default User <ec2-user@ip-172-31-28-224.ap-northeast-1.compute.internal>
2023-03-20 13:25:32 +0000

    [update]エラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    # def after_admin_sign_in_path_for(resource)
+    #   admin_posts_path
+    # end
+    
+    # def after_admin_sign_out_path_for(resource)
+    #   admin_session_path
+    # end
+    
+    def after_sign_in_path_for(resource)
+      if resource.class == Admin
+        admin_posts_path
+      else
+        root_path
+      end
+    end
+    
+    def after_sign_out_path_for(resource)
+      if resource == :admin
+        admin_session_path
+      else
+        root_path
+      end
+    end
+    
+    protected
+    
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in,keys: [:email])
+    end
 end
